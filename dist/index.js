3f9ad546540fdee0f4e70342223cfb94883d514c
nayon117 <hasibul.nayon1@gmail.com>
2024-03-15 17:21:49 +0600

    learn about array

--- dist/index.js
+++ dist/index.js
@@ -1,14 +1,23 @@
 "use strict";
 // let a:string="Hello World"; #1 way
-let b = "Hello World"; //another way
-let nambo;
-let isTrue;
+// let b = <string>"Hello World"; //another way
+// let nambo: number;
+// let isTrue: boolean;
 // union types
-let surname;
-surname = 117;
-// function
-const func = (n, m) => {
-    console.log(n, m);
-    return n * m;
-};
-let a = 13;
+// let surname: string | number;
+// surname = 117;
+// // function
+// type funcType= (n:number,m:number)=>number;
+// const func:funcType = (n,m) => {
+//   console.log(n, m);
+//   return n * m;
+// };
+// // type
+// type username = string | number;
+// let a:username=13
+// array
+let arr = [12, 24, 36, 48, 60];
+let arr2 = ["Hello", "World"];
+let arr3 = [1, 2, 3, 4, 5, "hello"];
+// tuple
+const arr4 = [1, "hello", true];
--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,28 @@
 // let a:string="Hello World"; #1 way
-let b = <string>"Hello World"; //another way
-let nambo: number;
-let isTrue: boolean;
+// let b = <string>"Hello World"; //another way
+// let nambo: number;
+// let isTrue: boolean;
 
 // union types
-let surname: string | number;
-surname = 117;
+// let surname: string | number;
+// surname = 117;
 
-// function
-const func = (n: number, m: number): number => {
-  console.log(n, m);
-  return n * m;
-};
+// // function
 
-// type
-type username = string | number;
-let a:username=13
+// type funcType= (n:number,m:number)=>number;
+// const func:funcType = (n,m) => {
+//   console.log(n, m);
+//   return n * m;
+// };
+
+// // type
+// type username = string | number;
+// let a:username=13
+
+// array
+let arr:number[] = [12,24,36,48,60];
+let arr2:string[] = ["Hello", "World"];
+let arr3:Array<number | string > = [1,2,3,4,5,"hello"];
+
+// tuple
+const arr4:[number, string, boolean] = [1, "hello", true];

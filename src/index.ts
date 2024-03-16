0504b70b9794f1363902b7661b98735a8f1f0095
nayon117 <hasibul.nayon1@gmail.com>
2024-03-16 09:15:16 +0600

    learn about objects in typescript

--- src/index.ts
+++ src/index.ts
@@ -20,9 +20,45 @@
 // let a:username=13
 
 // array
-let arr:number[] = [12,24,36,48,60];
-let arr2:string[] = ["Hello", "World"];
-let arr3:Array<number | string > = [1,2,3,4,5,"hello"];
+// let arr:number[] = [12,24,36,48,60];
+// let arr2:string[] = ["Hello", "World"];
+// let arr3:Array<number | string > = [1,2,3,4,5,"hello"];
 
 // tuple
-const arr4:[number, string, boolean] = [1, "hello", true];
+// const arr4:[number, string, boolean] = [1, "hello", true];
+
+// object
+// using type
+type objType = {
+    width:number,
+    height:number,
+    gender:boolean
+}
+
+const obj:objType = {
+    width:3434,
+    height:3434,
+    gender:true
+}
+
+// interfact type
+interface objType2 {
+    width:number,
+    height:number,
+    gender?:boolean
+}
+const obj2:objType2 = {
+    width:3434,
+    height:3434,
+
+}
+
+interface newObj extends objType2 {
+    color:string
+}
+
+const obj3:newObj ={
+    width:3434,
+    height:3434,
+    color:"red"
+}
